d1525ee7f2295c1e8fdd05ab025bd2887ea55422
Nikolay Okulov <okulov-nikolay@mail.ru>
2018-03-17 23:53:43 +0300

    some fix router for heroku #3

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const user = require('./users');
 const thread = require('./threads');
 const message = require('./messages');
 
+router.get('/', (req, res, next) => res.send('I am alive!'));
+
 router.use('/v1/user', user);
 router.use('/v1/thread', thread);
 router.use('/v1/message', message);
